9e39f2f0514a31013bca20b290ee99530fec0f51
Matteo Carniglia <matteo.carniglia@gmail.com>
2024-05-13 16:38:39 +0200

    final messages update

--- src/info/classes/FormValues.ts
+++ src/info/classes/FormValues.ts
@@ -19,7 +19,6 @@ import {
 export class IsMarriedForAdults implements ValidatorConstraintInterface {
   validate(married: any, args: ValidationArguments) {
     const age = (args.object as any).age;
-    console.log(age, married);
     return age < 18 || married != null;
   }
 
@@ -45,24 +44,24 @@ export class IsDateOfBirthValid implements ValidatorConstraintInterface {
   }
 
   defaultMessage() {
-    return 'The date of birth is not compatible with the age';
+    return 'The date of birth is not compatible with the age provided';
   }
 }
 
 export class FormValues {
   @MinLength(10, {
-    message: 'The name length must be between 10 and 50',
+    message: 'The name length must be between 10 and 50 characters',
   })
   @MaxLength(50, {
-    message: 'The name length must be between 10 and 50',
+    message: 'The name length must be between 10 and 50 characters',
   })
   @IsNotEmpty({
     message: 'The name is required',
   })
   name: string;
 
-  @Min(1, { message: 'The age must be between 1 and 150' })
-  @Max(150, { message: 'The age must be between 1 and 150' })
+  @Min(1, { message: 'The age must be between 1 and 150 years' })
+  @Max(150, { message: 'The age must be between 1 and 150 years' })
   @IsNumber()
   @IsNotEmpty({
     message: 'The age is required',
@@ -73,7 +72,13 @@ export class FormValues {
   married: boolean;
 
   @Validate(IsDateOfBirthValid)
-  @IsDateString({}, { message: 'The date format is not correct' })
+  @IsDateString(
+    {},
+    {
+      message:
+        'The date format is not correct. The expected format is ISO-8601 (YYYY-MM-DD)',
+    },
+  )
   @IsNotEmpty({
     message: 'The date of birth is required',
   })
